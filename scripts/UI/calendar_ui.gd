8d3e5a0d077745158470a1e5984d95b3de1e0726
Hessel <kylehessel@proton.me>
2023-10-12 16:45:06 -0700

    Added population of date_ui scenes to the calendar_ui's grid container and verified that things are showing up correctly, the count is right, and that the nodes are destroyed properly upon closure.

--- scripts/UI/calendar_ui.gd
+++ scripts/UI/calendar_ui.gd
@@ -2,7 +2,11 @@ extends CanvasLayer
 
 @onready var month_title: Label = $CalendarOutsideMargin/CalendarContainer/CalendarInsideMargin/CalendarVBox/TitleHBox/MonthTitle
 @onready var year_title: Label = $CalendarOutsideMargin/CalendarContainer/CalendarInsideMargin/CalendarVBox/TitleHBox/YearTitle
+var date_entry: PackedScene = preload("res://scenes/UI/date_ui.tscn")
 
 func _ready():
 	month_title.text = GameManager.calendar_manager.month.title
 	year_title.text = str(GameManager.calendar_manager.year.number)
+	
+	for d in range(GameManager.calendar_manager.month.days.size()):
+		%DaysGrid.add_child(date_entry.instantiate())
