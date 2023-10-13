5e4e36c756e685664afa072e91328d2064b82b5c
Hessel <kylehessel@proton.me>
2023-10-13 13:33:28 -0700

    Added color modulation to date_ui's date_panel when the given day in the grid matches the current in-game day.

--- scripts/UI/calendar_ui.gd
+++ scripts/UI/calendar_ui.gd
@@ -9,6 +9,10 @@ func _ready():
 	year_title.text = str(GameManager.calendar_manager.year.number)
 	
 	for d in range(GameManager.calendar_manager.month.days.size()):
+		var current_day: int = GameManager.calendar_manager.current_day_num
 		var date_inst: Control = date_entry.instantiate()
 		%DaysGrid.add_child(date_inst)
+		
 		date_inst.date_number.text = str(d + 1)
+		if d == current_day:
+			date_inst.date_panel.modulate = "d6b247"
--- scripts/UI/date_ui.gd
+++ scripts/UI/date_ui.gd
@@ -2,6 +2,7 @@ extends Control
 
 class_name DateUI
 
+@onready var date_panel: PanelContainer = $DatePanel
 @onready var date_number: Label = $DatePanel/DatePanelMargin/DateNumber
 
 func _ready():
