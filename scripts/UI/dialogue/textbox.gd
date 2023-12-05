c7db3be0c1d749b3b5dd6a8ad6b08b506cc4cc0b
Polluted Mind <pollutedmind@proton.me>
2023-12-05 16:51:42 -0700

    Reconfigured the textbox scene.

--- scripts/UI/dialogue/textbox.gd
+++ scripts/UI/dialogue/textbox.gd
@@ -1,1 +1,6 @@
 extends CanvasLayer
+
+@onready var TextboxMargin: MarginContainer = $TextboxMargin
+@onready var TextboxPanel: PanelContainer = $TextboxMargin/TextboxPanel
+@onready var DialogueLabel: Label = $LetterDisplayTimer
+
