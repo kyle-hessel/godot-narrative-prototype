2cb57c944dd4129c1a000fa1d9be17fb133c0bb5
PollutedMind <pollutedmind@proton.me>
2023-12-10 22:44:43 -0700

    This is a sample commit that shows how to hide text using a RichTextEffect and the TextServer. I am going to switch the text display_letter technique to live on a RichTextEffect, as the current technique isn't compatible with RichTextEffects, which kills the point of using them.

--- scripts/UI/dialogue/text/dialogue_label_color.gd
+++ scripts/UI/dialogue/text/dialogue_label_color.gd
@@ -2,6 +2,13 @@ extends RichTextEffect
 
 var bbcode := "highlight_color"
 
+# Gets TextServer for retrieving font information.
+func get_text_server():
+	return TextServerManager.get_primary_interface()
+
 func _process_custom_fx(char_fx: CharFXTransform) -> bool:
 	char_fx.color=char_fx.env.get("color")
+	var char_int: int = get_text_server().font_get_char_from_glyph_index(char_fx.font, 1, char_fx.glyph_index)
+	if char_int == "a".unicode_at(0):
+		char_fx.visible = false
 	return true
