80a5afb001dccab064632b9e502ea00ed1d71ce3
PollutedMind <pollutedmind@proton.me>
2023-12-14 21:31:37 -0700

    Refactored and fixed a multitude of bugs with the cutscene system and smooth continuation of actions throughout events and events throughout cutscenes. I am liking this system quite a lot!

--- scripts/systems/cutscene.gd
+++ scripts/systems/cutscene.gd
@@ -14,15 +14,16 @@ var action_index: int = 0
 
 # send a signal when the cutscene finishes to send to the save system.
 # the save system will then determine on boot which cutscenes should be monitoring for a cutscene trigger.
-signal finished
+signal cutscene_finished
+signal event_finished
 
 func _ready() -> void:
 	GameManager.events_manager.register_cutscene(self)
 	
-	# signal connections for continuing cutscenes
-	anim_player.animation_finished.connect(func(anim_name: StringName): 
-		play_action(events[event_index].actions[action_index])
-		)
+	# signal connections for continuing cutscenes, and finishing events and cutscenes
+	anim_player.animation_finished.connect(func(anim_name: StringName): increment_action())
+	cutscene_finished.connect(func(): event_index = -1)
+	event_finished.connect(func(): action_index = 0)
 
 func switch_camera(cam_name: String) -> void:
 	pass
@@ -33,21 +34,26 @@ func _on_cutscene_area_body_entered(body):
 func start_cutscene() -> void:
 	# fetches the node from an assigned NodePath.
 	get_node(cameras["main"]).current = true
-	play_event(events[event_index])
+	continue_cutscene()
 
 func continue_cutscene() -> void:
-	if event_index < events.size() - 1:
-		event_index += 1
-		play_event(events[event_index])
+	if event_index < events.size() && event_index > -1:
+		continue_event()
+	elif event_index < 0: # this means the cutscene has already been played before
+		pass
+	else:
+		cutscene_finished.emit()
 
-func play_event(event: Event) -> void:
-	play_action(event.actions[action_index])
-	if action_index < event.actions.size() - 1:
-		action_index += 1
+func continue_event() -> void:
+	if action_index < events[event_index].actions.size():
+		play_action(events[event_index].actions[action_index])
+	else:
+		event_index += 1
+		event_finished.emit()
+		continue_cutscene()
 
 func play_action(action: Action) -> void:
 	print(action)
-	print(typeof(action))
 	var action_context = action.action.keys()[0]
 	if action_context is Animation:
 		var anim_str: StringName = anim_player.find_animation(action_context)
@@ -69,12 +75,17 @@ func play_action(action: Action) -> void:
 		var node = get_node(action_context)
 		if node is Camera3D:
 			get_node(action_context).current = true
-			print(get_node(action_context).current)
 			# different potential camera movement
 			# one potential way to switch cameras AND animate.
 			# could just keep this up in the above Animation check
 			# instead, and see what object is in the anim track?
 			if action.action[action_context] is Animation:
 				pass
+			
+			increment_action()
 	else:
 		pass
+
+func increment_action() -> void:
+	action_index += 1
+	continue_event()
