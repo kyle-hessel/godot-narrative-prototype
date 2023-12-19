c103d77e4999181b1f46379cd60ef03d49a59a3e
PollutedMind <pollutedmind@proton.me>
2023-12-18 22:04:10 -0700

    Did some tests: cutscene AnimationPlayer data cannot be reused in a generic way across different scenes, at least not in its current form. This isn't a huge deal anyway, as that animation data itself is in world space, it's not in a local space relative to the cutscene itself, and depending on the scene generic reusable cutscenes would likely need a lot of tweaking anyhow. This doesn't mean that I still can't make reusable event logic, load that into many cutscenes, and then tweak it on the fly as necessary - the system is still incredibly modular and allows for this, some things will just have to be handcrafted every time (for now). Even so, with clever use of stuff such as dictionaries, node groups, and Animation.track_set_path (see the Godot Docs) what I have in mind definitely is still possible in the situations where I want that functionality - what this would be especially useful for in the generic sense is swapping out the character that appears in a cutscene doing a certain action, depending on the player's choices up until that point. Also related to cutscenes, I added a cutscene load to level.gd's _ready function to show that once a cutscene is configured, it can be made local, saved as a scene, and then loaded back into the game at an arbitrary point during runtime and it will still function exactly as expected. This is great, as it will allow for a staggered cutscene load system depending on the player's point in the story, which will drastically reduce both memory load at runtime, and make the editor easy to navigate; once I properly configure a cutscene I can just delete it from the scene for the time being.

--- scripts/level.gd
+++ scripts/level.gd
@@ -2,10 +2,22 @@ extends Node3D
 
 var physics_tick: int = ProjectSettings.get_setting("physics/common/physics_ticks_per_second")
 
+signal cutscene_ready
+
 func _ready():
 	randomize()
 	Engine.max_fps = physics_tick
 	OS.delta_smoothing = true
+	
+	cutscene_ready.connect(func(): print("cutscene loaded!"))
+	
+	spawn_cutscene()
+
+func spawn_cutscene() -> void:
+	var test_cutscene: PackedScene = load("res://assets/cutscenes/samples/cutscene_test.tscn")
+	var test_cutscene_inst: Node3D = test_cutscene.instantiate()
+	add_child(test_cutscene_inst)
+	cutscene_ready.emit()
 
 func _physics_process(delta):
 	match physics_tick:
