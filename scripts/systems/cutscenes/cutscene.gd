355ccfc269b5027f6e97b2fbe3153011c08d5286
PollutedMind <pollutedmind@proton.me>
2023-12-22 21:15:00 -0700

    Further experimented and troubleshooted sub-actions and parallel actions: Sub-actions are working well now. As it turns out, I had already solved my own problem. Sub-actions themselves can run action contexts in parallel due to their implementation being almost identical to normal actions, so the need for running them alongside normal action contexts isn't necessary. If a sub-action AND a parallel action context is required, said action contexts can simply be added to the sub-action, and multiple sub-actions can run in a row as well for more granularity. Further, any normal actions after should still have to await all sub-actions to complete, so I think this system is well-designed (but it does require more testing). Overall, I'm happy with this approach.

--- scripts/systems/cutscenes/cutscene.gd
+++ scripts/systems/cutscenes/cutscene.gd
@@ -103,7 +103,6 @@ func play_action(action: Action, is_subaction: bool = false) -> void:
 	if subactions_active && !is_subaction:
 		await subactions_finished
 	
-	# FIXME: make sub-actions work with parallel actions.
 	if is_subaction:
 		subaction_contexts = action.action.keys()
 		
